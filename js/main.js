331c7016b84b9e5d50641a1394411a6609d5f429
ashan012 <khan89891918@outlook.com>
2025-06-08 01:50:13 +0500

    implement async await function

--- js/main.js
+++ js/main.js
@@ -1,40 +1,32 @@
 //https://randomuser.me/api/?results=2
 
-const api = () => {
-  return new Promise((res, rej) => {
-    res(fetch("https://randomuser.me/api/?results=5"));
-    rej("fail to fethch");
-  });
+const api = async () => {
+  return await fetch("https://randomuser.me/api/?results=5");
 };
 document.getElementById("refresh").addEventListener("click", () => {
   location.reload();
 });
 
-const callApi = () => {
+const callApi = async () => {
   let firstname = [];
   let email = [];
   let phone = [];
   let img = [];
-  let data = api();
-  data
-    .then((res) => res.json())
-    .then((res) => {
-      for (let i = 0; i < 5; i++) {
-        firstname.push(res.results[i].name.first);
-        email.push(res.results[i].email);
-        phone.push(res.results[i].phone);
-        img.push(res.results[i].picture.large);
+  let data = await api();
+  data = await data.json();
+  for (let i = 0; i < 5; i++) {
+    console.log(data.results[i].name.first);
+    firstname.push(data.results[i].name.first);
+    email.push(data.results[i].email);
+    phone.push(data.results[i].phone);
+    img.push(data.results[i].picture.large);
 
-        document.getElementsByClassName("firstName")[i].innerText =
-          firstname[i];
+    document.getElementsByClassName("firstName")[i].innerText = firstname[i];
 
-        document.getElementsByClassName("email")[i].innerText = email[i];
-        document.getElementsByClassName("phone")[i].innerText = phone[i];
-        document.getElementsByClassName("img")[i].src = img[i];
-      }
-
-      console.log(firstname, email, phone, img);
-    });
+    document.getElementsByClassName("email")[i].innerText = email[i];
+    document.getElementsByClassName("phone")[i].innerText = phone[i];
+    document.getElementsByClassName("img")[i].src = img[i];
+  }
 };
 
 callApi();
